2fefc0ff8419a00b771b2d2baa63213f68fa934a
AbhijeetRajak <rajakabhijeet6@gmail.com>
2021-03-21 23:59:51 +0530

    Done

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,8 @@ import store from "./redux/store";
 import Login from "../src/components/auth/Login";
 import Register from "../src/components/auth/Register";
 import Dashboard from "../src/pages/Dashboard";
-import UserTransactionList from '../src/components/AllUserList/UserTransactionList';
+import UserTransactionList from "../src/components/AllUserList/UserTransactionList";
+import RouteProtection from "../src/components/RouteProtection/RouteProtection";
 
 function App() {
   return (
@@ -14,12 +15,17 @@ function App() {
       <Provider store={store}>
         <div className="App"></div>;
         <Switch>
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/dashboard" component={Dashboard}/>
-          <Route exact path="/user-transaction-list" component={UserTransactionList}/>
-        </Switch>
+          {/* <Route path="/"/> */}
+          <RouteProtection exact path="/login" component={Login} />
+          <RouteProtection exact path="/register" component={Register} />
+          <RouteProtection exact path="/dashboard" component={Dashboard} />
+          <RouteProtection
+            exact
+            path="/user-transaction-list"
+            component={UserTransactionList}
+          />
         <Redirect to="/login" />
+        </Switch>
       </Provider>
     </BrowserRouter>
   );
